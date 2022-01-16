e9fb1fe1fd33ae6eddbe1ae164f4c8e79b3a319c
Kit Plummer <kitplummer@gmail.com>
2022-01-16 17:28:45 -0500

    testin silence

--- hello.sh
+++ hello.sh
@@ -1,4 +1,4 @@
 echo "TEST MULTILINE"
 echo "commit: ${GOA_LAST_COMMIT_ID} 22"
 echo "time: ${GOA_LAST_COMMIT_TIME}"
-echo "author: ${GOA_LAST_COMMIT_AUTHOR}"
+echo -n "author: ${GOA_LAST_COMMIT_AUTHOR}"
